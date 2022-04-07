9946e2c517a14da3c936159784bcdc9a694091ef
OctavioIorio <octaviomauroariel_iorio@iescarlesvallbona.cat>
2022-04-07 16:59:33 +0200

    Navbar start

--- src/index.js
+++ src/index.js
@@ -2,13 +2,28 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+//import Navbar from './Navbar';
+import Home from './Home';
+//import Footer from './Footer';
 import reportWebVitals from './reportWebVitals';
 
+// const navbar = ReactDOM.createRoot(document.getElementById('navbar'));
+// const home = ReactDOM.createRoot(document.getElementById('root'));
+// navbar.render(<App />);
+// home.render(<Home />);
+
 ReactDOM.render(
-  <React.StrictMode>
+  <>
     <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  </>,
+  document.getElementById("navbar")
+);
+
+ReactDOM.render(
+  <>
+    <Home />
+  </>,
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
